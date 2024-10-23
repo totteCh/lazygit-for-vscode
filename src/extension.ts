d857db934b838b8739406bea7d05a28bb7d8fbe1
Christoffer Wigforss <tottech@gmail.com>
2024-10-23 13:46:05 +0200

    fix: prepend space to command if space ignored in history

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
 import * as vscode from "vscode";
+import * as child_process from "child_process";
+import * as process from "process";
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
@@ -29,6 +31,31 @@ async function focusActiveLazygitInstance(): Promise<boolean> {
   return false;
 }
 
+// Function to detect shell and check if the HISTIGNORE_SPACE option is enabled
+function shouldPrependSpace() {
+  // Detect shell type
+  const shell = process.env.SHELL;
+
+  try {
+    if (shell?.endsWith("zsh")) {
+      // Check for histignorespace in zsh
+        const result = child_process
+          .execSync("/bin/zsh -c 'source ~/.zshrc; setopt'")
+          .toString();
+        return result.includes("histignorespace");
+    } else if (shell?.endsWith("bash")) {
+      // Check if HISTCONTROL includes ignorespace in bash
+      const result = child_process
+        .execSync("/bin/bash -c 'source ~/.bashrc; echo $HISTCONTROL'")
+        .toString();
+      return result.includes("ignorespace");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  return false; // Default to no space if detection fails
+}
+
 async function newLazygitInstance() {
   // Always create a new terminal
   await vscode.commands.executeCommand(
@@ -36,7 +63,13 @@ async function newLazygitInstance() {
   );
 
   let terminal = vscode.window.activeTerminal!;
-  terminal.sendText("lazygit && exit");
+  // Use the check and send the command
+  const prependSpace = shouldPrependSpace();
+  let command = "lazygit && exit";
+  if (prependSpace) {
+    command = ` ${command}`; // Prepend space if the option is enabled
+  }
+  terminal.sendText(command);
   terminal.show();
 
   // Move the terminal to the editor area
